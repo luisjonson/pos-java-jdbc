912b2a5f8f7cbe9b8384f0ae8ce3d55044d24d55
jonson <luizlindonjonson@yahoo.com.br>
2019-12-10 23:30:19 -0300

    criando o metodo salvar no USERPOSDAO

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,13 +2,20 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conxaojdbc.SingleConnection;
+import dao.UserPosDao;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDao userPosDao = new UserPosDao();
+		Userposjava userPosJava = new Userposjava();
+
+		userPosJava.setId(8L);
+		userPosJava.setNome("carlos");
+		userPosJava.setEmail("carlos@gmail.com");
+		userPosDao.salva(userPosJava);
 	}
 
 }
