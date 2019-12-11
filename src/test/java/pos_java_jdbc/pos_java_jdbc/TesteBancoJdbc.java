9563cc48883615c10a266f6bb7747e2d454741f5
jonson <luizlindonjonson@yahoo.com.br>
2019-12-11 00:09:33 -0300

    criando o metodo LISTAR que busca uma lista de usuario, e o metodo BUSCAR que traz um usuario pelo id

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,5 +1,7 @@
 package pos_java_jdbc.pos_java_jdbc;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import dao.UserPosDao;
@@ -18,4 +20,32 @@ public class TesteBancoJdbc {
 		userPosDao.salva(userPosJava);
 	}
 
+	@Test
+	public void initListar() {
+		UserPosDao dao = new UserPosDao();
+		try {
+			List<Userposjava> list = dao.listar();
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("__________________________________________");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void initBuscar() {
+		UserPosDao dao = new UserPosDao();
+		try {
+			Userposjava userposjava = dao.buscar(6L);
+
+			System.out.println(userposjava);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
